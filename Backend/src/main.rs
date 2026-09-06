7c166cd6f978e7764a0001e882300b670667f773
Tyler Long <aethdae@gmail.com>
2026-09-06 03:25:56 -0500

    found solution for generic datatypes in backend

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,16 +1,39 @@
-use actix_web::{App, Error, HttpServer, Responder, get, web};
+use actix_web::{App, Error, HttpServer, Responder, get, web::{self}};
 use serde::Serialize;
+use serde_json::Value;
 
 
 #[derive(Serialize)]
-struct MyObj{
-    name: String
+struct Data{
+    name: String,
+    data: Value
 }
 
-#[get("/a/{name}")]
+#[derive(Serialize)]
+struct Healthy{
+    health: String
+}
+
+#[get("/")]
+async fn health() -> Result<impl Responder, Error> {
+    let health = Healthy {
+        health: "Ok".to_string()
+    };
+    Ok(web::Json(health))
+}
+
+#[get("/{name}")]
 async fn index(name: web::Path<String>) -> Result<impl Responder, Error> {
-    let obj = MyObj {
+    let str = r#"
+    {
+    "test": "test1", 
+    "test2": "test3"
+    }
+    "#;
+    let v: Value = serde_json::from_str(str)?;
+    let obj = Data {
         name: name.to_string(),
+        data: v
     };
     Ok(web::Json(obj))
 }
@@ -18,8 +41,8 @@ async fn index(name: web::Path<String>) -> Result<impl Responder, Error> {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     
-    HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
+    HttpServer::new(|| App::new().service(index).service(health))
+        .bind(("127.0.0.1", 3000))?
         .run()
         .await
 }
