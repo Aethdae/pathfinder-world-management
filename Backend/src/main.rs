b456f16f6f9a2d3f5bfdcedd21c349289688d371
Tyler Long <aethdae@gmail.com>
2026-09-09 02:45:57 -0500

    post route connected to DB, errors on handling data type: NF

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -1,10 +1,10 @@
-use actix_web::{App, Error, HttpServer, Responder, get, post, web::{self}};
+use actix_web::{App, Error, HttpServer, Responder, get, post, web::{self, Json}};
 use serde::Serialize;
 use serde_json::Value;
 use sqlite;
 
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 struct Data{
     name: String,
     data: Value
@@ -40,9 +40,22 @@ async fn index(name: web::Path<String>) -> Result<impl Responder, Error> {
 
 #[post("/{name}")]
 async fn add_new(name: web::Path<String>, data: web::Data<String>) -> Result<impl Responder, Error> {
+    let connection   = sqlite::open("pf.db").unwrap();
+    println!("{}, {:?}", name, data);
+    let data_str = serde_json::to_string(&data).expect("Failed to unpack Json");
+    let command_string = format!("
+    CREATE TABLE IF NOT EXISTS world(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    data TEXT);
+    INSERT INTO world(name, data)
+    VALUES({}, {:?})
+    ", name, data_str);
+    let exe = connection.execute(command_string).unwrap();
+    println!("{:?}, {}", exe, name);
     let data = Data{
         name: name.to_string(),
-        data: serde_json::from_str(&data)?
+        data: serde_json::Value::String(data_str)
     };
     Ok(web::Json(data))
 }
@@ -50,7 +63,10 @@ async fn add_new(name: web::Path<String>, data: web::Data<String>) -> Result<imp
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     
-    HttpServer::new(|| App::new().service(index).service(health))
+    HttpServer::new(|| App::new()
+        .service(index)
+        .service(health)
+        .service(add_new))
         .bind(("127.0.0.1", 3000))?
         .run()
         .await
